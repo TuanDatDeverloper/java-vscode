e5471e905bb74d983326432a224e941388d00a03
TuanDatDeverloper <datnst.24it@vku.udn.vn>
2024-11-28 10:44:15 +0700

    first commit

--- javaproject1/Student.java
+++ javaproject1/Student.java
@@ -1,35 +1,10 @@
+import java.util.Scanner;
 public class Student {
-    // Các biến thành viên
-    private String name;
-    private double javaScore;
-    private double cScore;
-
-    // Constructor để khởi tạo đối tượng sinh viên
-    public Student(String name, double javaScore, double cScore) {
-        this.name = name;
-        this.javaScore = javaScore;
-        this.cScore = cScore;
-    }
-
-    // Phương thức để hiển thị thông tin sinh viên và điểm trung bình
-    public void displayInfo() {
-        double average = (javaScore + cScore) / 2;
-        System.out.println("Tên Sinh Viên: " + name);
-        System.out.println("Điểm Java: " + javaScore);
-        System.out.println("Điểm C Programming: " + cScore);
-        System.out.printf("Điểm Trung Bình: %.2f\n", average);
-        System.out.println("-----------------------------");
-    }
-
     public static void main(String[] args) {
-        // Tạo đối tượng sinh viên thứ nhất
-        Student student1 = new Student("Nguyen Van A", 85.5, 92.0);
-        
-        // Tạo đối tượng sinh viên thứ hai
-        Student student2 = new Student("Nguyen Van B", 78.0, 88.5);
-        
-        // Hiển thị thông tin cho mỗi sinh viên
-        student1.displayInfo();
-        student2.displayInfo();
-    }
+		Scanner dat = new Scanner(System.in);
+		System.out.println("Enter your name: ");
+		String name = dat.nextLine();
+		System.out.println("Your name is: " + name);
+		dat.close();
+	}
 }
